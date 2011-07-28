1b97557c171b26431567b61e26c0ff0b4938e205
Pete Nicholls <pete@metanation.com>
2011-07-28 14:40:35 +1200

    Added lipogram and pangram detection.

--- lib/oulipo.rb
+++ lib/oulipo.rb
@@ -1,3 +1,19 @@
 class Oulipo
+  ALPHABET = 'a'..'z'
   
+  def self.absent_letters(string)
+    present_letters = string.downcase.split('').uniq
+    missing_letters = ALPHABET.to_a - present_letters
+    missing_letters.empty? ? nil : missing_letters
+  end
+  
+  # A pangram uses all letters
+  def self.pangram?(string)
+    self.absent_letters(string).nil?
+  end
+  
+  # A lipogram has missing letters
+  def self.lipogram?(string)
+    !self.pangram?(string)
+  end
 end
---
+++ spec/lipograms_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "lipograms and pangrams" do
+  
+  # Easy to switch class, if refactored
+  let(:performer) { Oulipo }
+  
+  let(:pangram) { 'The quick brown fox jumps over a lazy god' }
+  let(:lipogram) { 'The quick grey fox jumps over lazy gods' }
+  
+  it "shows absent letters" do
+    performer.absent_letters(pangram).should == nil
+    performer.absent_letters(lipogram).should == %w{ b n w }
+  end
+  
+  it "can tell a pangram apart from a lipogram" do
+    performer.lipogram?(lipogram).should == true
+    performer.lipogram?(pangram).should == false
+    
+    performer.pangram?(pangram).should == true
+    performer.pangram?(lipogram).should == false
+  end
+  
+end
